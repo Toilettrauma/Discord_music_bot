6840aba301e9b81f954bbf030e43db90f0c65e25
Toilettrauma <macosinternal@gmail.com>
2024-04-22 01:23:11 +0300

    Add Readme and ydl_patch

---
+++ PATCH/patch_ydl.py
@@ -0,0 +1,36 @@
+import subprocess
+import os
+import re
+
+if __name__ == "__main__":
+	package_info = subprocess.check_output("pip show youtube_dl --no-color").decode("utf-8")
+	if package_info.startswith("WARNING"):
+		print("ERROR: youtube_dl not found")
+
+	ydl_location = None
+	for info_line in package_info.split("\r\n"):
+		if info_line.startswith("Location"):
+			ydl_location = info_line.split("Location: ")[1]
+			break
+	else:
+		print("ERROR: Location line not found in 'pip show'")
+
+	ydl_path = os.path.join(ydl_location, "youtube_dl\\extractor")
+	uploader_id_patched = False
+	with open(os.path.join(ydl_path, "youtube.py"), encoding="utf-8") as in_fp, \
+		 open(os.path.join(ydl_path, "youtube_patched.py"), "w", encoding="utf-8") as out_fp:
+		for line in in_fp:
+			if "'uploader_id': self._search_regex(r'/(?:channel|user)" in line:
+				print("PATCH: uploader_id")
+				uploader_id_patched = True
+				line = line.replace("(?:channel|user)/", "@")
+			out_fp.write(line)
+
+	if not uploader_id_patched:
+		print("ERROR: failed to patchfind")
+		os.remove(os.path.join(ydl_path, "youtube_patched.py"))
+
+	os.remove(os.path.join(ydl_path, "youtube.py"))
+	os.rename(os.path.join(ydl_path, "youtube_patched.py"), os.path.join(ydl_path, "youtube.py"))
+
+	print("SUCCESS: patched")
---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Discord music bot (RU)
+## Table of Contents
+* [Features](#features)
+* [Using](#using)
+
+
+## Features
+Play music in channels with ANSI queue and buttons
+* equalizer (needs to be improved)
+* some settings (no multiguild)
+
+## Using
+1. (Optional) create venv
+```
+python -m venv venv
+venv\Scripts\activate
+```
+2. Install all requirements
+```
+pip install -r requirements.txt
+```
+3. (Optional) place `cookies.txt` for youtube_dl (NETSCAPE)
+4. run `ydl_patch.py` in PATCH folder
+5. Rename `Secrets.py.placeholder` to `Secrets.py` and fill it with your data
+6. Run `Bot.py`
